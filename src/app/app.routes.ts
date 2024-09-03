9a7025bcf3cafc75eed335443b6f678d592a49ac
EmilseBilse <bandittenxd@gmail.com>
2024-09-03 11:12:23 +0200

    background for login and other pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { OverviewPageComponent } from './pages/overview-page/overview-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: 'login', component: LoginPageComponent },
+    { path: 'overview', component: OverviewPageComponent },
+];
---
+++ src/app/pages/overview-page/overview-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-overview-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './overview-page.component.html',
+  styleUrl: './overview-page.component.scss'
+})
+export class OverviewPageComponent {
+
+}
