219433d383bf729bd06710abc3fd7bf8ca462f49
Michele <msa@danskscanning.dk>
2024-09-06 08:45:11 +0200

    Table overview

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { OverviewComponent } from './overview/overview.component';
+import { CreateResponseComponent } from './create-response/create-response.component';
+import { ViewEventComponent } from './view-event/view-event.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'overview', component: OverviewComponent },
+    { path: 'view-event', component: ViewEventComponent },
+    { path: 'create-response', component: CreateResponseComponent },
+];
