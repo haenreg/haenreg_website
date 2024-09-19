53f8f9f84cb8f5df33ddd8a361760937ad068c84
EmilseBilse <71692416+EmilseBilse@users.noreply.github.com>
2024-09-19 13:58:03 +0200

    save

--- src/app/interfaces/iQuestion.ts
+++ src/app/interfaces/iQuestion.ts
@@ -4,6 +4,15 @@ export interface Question {
   id: number;
   title: string;
   description: string;
-  type: string;
+  type: QuestionType;
   questionChoices: QuestionChoice[];
+}
+
+export enum QuestionType {
+  Date = 'DATE',
+  Text = 'TEXT',
+  SelectOne = 'SELECT_ONE',
+  MultiSelect = 'MULTI_SELECT',
+  Scale = 'SCALE',
+  YesNo = 'YES_NO',
 }
--- src/app/pages/administrate-question/administrate-question.component.ts
+++ src/app/pages/administrate-question/administrate-question.component.ts
@@ -1,22 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { QuestionService } from '../../services/question.service';
-import { Question } from '../../interfaces/iQuestion';
+import { Question, QuestionType } from '../../interfaces/iQuestion';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-administrate-question',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './administrate-question.component.html',
   styleUrl: './administrate-question.component.scss',
 })
 export class AdministrateQuestionComponent implements OnInit {
   private question: Question;
-  constructor(private questionService: QuestionService) {}
+  public formGroup: FormGroup;
+  questionTypes = Object.values(QuestionType);
+
+  constructor(
+    private questionService: QuestionService,
+    private fb: FormBuilder
+  ) {}
 
   ngOnInit() {
     this.question = this.questionService.getQuestion();
 
-    console.log(this.question);
+    this.formGroup = this.fb.group({
+      title: new FormControl(this.question ? this.question.title : ''),
+      description: new FormControl(
+        this.question ? this.question.description : ''
+      ),
+      type: new FormControl(
+        this.question ? this.question.type : QuestionType.Text
+      ),
+    });
   }
 }
