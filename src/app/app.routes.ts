465babcd36964da6ae32b646cf249393308a7295
EmilseBilse <bandittenxd@gmail.com>
2024-09-20 13:36:54 +0200

    cleanup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { authGuard } from './auth.guard';
-import { CreateResponseComponent } from './pages/create-response/create-response.component';
-import { ViewEventComponent } from './pages/view-event/view-event.component';
 import { OverviewComponent } from './pages/overview/overview.component';
-import { QuestionOverviewComponent } from './pages/question-overview/question-overview.component';
-import { FieldsComponent } from './components/fields/fields.component';
-import { ScaleComponent } from './components/scale/scale.component';
-import { YesNoComponent } from './components/yes-no/yes-no.component';
 import { DetailsPageComponent } from './pages/details-page/details-page.component';
 import { ManageQuestionsPageComponent } from './pages/manage-questions-page/manage-questions-page.component';
 import { AdministrateQuestionComponent } from './pages/administrate-question/administrate-question.component';
@@ -30,11 +24,5 @@ export const routes: Routes = [
     path: 'administrate-question',
     component: AdministrateQuestionComponent,
     canActivate: [authGuard],
-  },
-  { path: 'view-event', component: ViewEventComponent },
-  { path: 'create-response', component: CreateResponseComponent },
-  { path: 'fields', component: FieldsComponent },
-  { path: 'scale', component: ScaleComponent },
-  { path: 'yes-no', component: YesNoComponent },
-  { path: 'questions', component: QuestionOverviewComponent, canActivate: [authGuard] },
+  }
 ];
